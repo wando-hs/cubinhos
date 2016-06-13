bca180b57621f0c77d1cd516cba47eee38352a6a
Fernando Oliveira <fernando@fholiveira.com>
2016-06-13 18:51:53 -0300

    Create sutb data for tests

---
+++ test/Stubs.hs
@@ -0,0 +1,46 @@
+module Stubs where
+
+import Face
+import Side
+
+a1 = A [False, False, True, False, True]
+b1 = B [True, True, False, False, False]
+c1 = C [False, True, False, True, False]
+d1 = D [False, True, False, True, False]
+
+face1 = Face a1 b1 c1 d1
+
+a2 = A [True, True, True, False, False]
+b2 = B [False, False, True, False, True]
+c2 = C [True, True, False, True, False]
+d2 = D [False, False, True, False, True]
+
+face2 = Face a2 b2 c2 d2
+
+a3 = A [False, False, True, False, False]
+b3 = B [False, False, True, False, True]
+c3 = C [True, True, False, True, False]
+d3 = D [False, True, False, True, False]
+
+face3 = Face a3 b3 c3 d3
+
+a4 = A [True, False, True, False, False]
+b4 = B [False, True, False, True, True]
+c4 = C [True, False, True, False, True]
+d4 = D [True, True, False, True, True]
+
+face4 = Face a4 b4 c4 d4
+
+a5 = A [False, False, True, False, False]
+b5 = B [False, True, False, True, True]
+c5 = C [True, False, True, False, False]
+d5 = D [False, True, False, True, False]
+
+face5 = Face a5 b5 c5 d5
+
+a6 = A [False, False, True, False, False]
+b6 = B [False, True, False, True, False]
+c6 = C [False, True, False, True, False]
+d6 = D [False, False, True, False, False]
+
+face6 = Face a6 b6 c6 d6
